11f1c155aef88c9b3bce13db521521e958837f3d
dawijaya <113819824+dawijaya@users.noreply.github.com>
2024-03-01 11:22:18 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,26 @@
 // index.jsx
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Mengganti Switch dengan Routes
-import App from './App'; // Ubah nama import dari 'MainApp' menjadi 'App'
-import Apps12 from './Apps12'; // Ubah nama import dari 'Apps12' menjadi 'Apps12'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import App from './App';
+import Apps12 from './Apps12';
 
 function Navigation() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-          <Link to="/" className="link-to-home">
-          <img src="https://github.com/dawijaya/toko_online/blob/main/public/image/home.jpeg?raw=true" alt="Home" />
-          <span>Home</span>
-          </Link>
-          </ul>
-        </nav>
-        <Routes> {/* Gunakan Routes sebagai pembungkus */}
-          <Route path="/" element={<App />} />
+        <Routes>
+          <Route path="/" element={<App />} /> {/* Letakkan ini di atas */}
           <Route path="/cart" element={<Apps12 />} />
         </Routes>
+        <nav>
+          <ul>
+            <Link to="/" className="link-to-home">
+              <img src="https://github.com/dawijaya/toko_online/blob/main/public/image/home.jpeg?raw=true" alt="" />
+              <span>Home</span>
+            </Link>
+          </ul>
+        </nav>
       </div>
     </Router>
   );
